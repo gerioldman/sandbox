d2ff5faac77f1c13f7d4e7a8a2531e76643feded
gerioldman <k.gergo49@gmail.com>
2023-06-30 21:58:41 +0200

    Replace hungarian comments

--- units/Complementary_Filter/Unit/src/Complementary_Filter.c
+++ units/Complementary_Filter/Unit/src/Complementary_Filter.c
@@ -48,31 +48,37 @@ void Complementary_Filter_UpdateFilter(const float accelerometer[3], const float
     float phiHat_acc_rad = atan2f(accelerometer[1], accelerometer[2]); // atanf(facc[1] / facc[2]
     float thetaHat_acc_rad = asinf(accelerometer[0] / 10.0f);
 
-    // Body rates -> Euler rates, Föld inerciarendszerbe transzormálás
+    // Body rates -> Euler rates, transform into Earth frame
     float phiDot_rps = gyroscope[0] + tanf(thetaHat) * (sinf(phiHat) * gyroscope[1] + cosf(phiHat) * gyroscope[2]);
     float thetaDot_rps = cosf(phiHat) * gyroscope[1] - sinf(phiHat) * gyroscope[2];
 
-    // Előző adat lementése ha hiba történne
+    // Save old data if error occurs
     float oldphi = phiHat;
 
-    // Becslés
+    // Estimation
     phiHat = COMP_FILT_ALPHA * phiHat_acc_rad + (1.0f - COMP_FILT_ALPHA) * (phiHat + (1 / DATA_FREQUENCY) * phiDot_rps);
 
-    // Hiba ellenőrzése és adat eldobása, ha hiba van
+    // Check if error occured and discard data if error occured
     if (isnan(phiHat))
         phiHat = oldphi;
 
-    // Előző adat lementése ha hiba történne
+    // Save old data if error occurs
     float oldtheta = thetaHat;
 
-    // Becslés
+    // Estimation
     thetaHat = COMP_FILT_ALPHA * thetaHat_acc_rad + (1.0f - COMP_FILT_ALPHA) * (thetaHat + (1.0f / DATA_FREQUENCY) * thetaDot_rps);
 
-    // Hiba ellenőrzése és adat eldobása, ha hiba van
+    // Check if error occured and discard data if error occured
     if (isnan(thetaHat))
         thetaHat = oldtheta;
 }
 
+
+/**
+ * @brief Returns the current estimated orientation data
+ * 
+ * @return Orientation_Data_t 
+ */
 Orientation_Data_t Complementary_Filter_GetOrientationData()
 {
     return ((Orientation_Data_t){ .phiHat = phiHat , .thetaHat = thetaHat });
